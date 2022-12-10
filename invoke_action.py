40a54406a9c0673cd6f7ce55c07739e980cdcf1d
qi0523 <1426042225@qq.comclear>
2022-12-10 23:02:32 +0800

    invoke action

--- invoke_action.py
+++ invoke_action.py
@@ -6,16 +6,20 @@ interval = sys.argv[1]
 
 file = open("./benchmark/" + interval + "-hours/invoke_actions.sh", "r")
 
+lines = file.readlines()
+
+for i in range(len(lines)):
+    if lines[i][-1] == '\n':
+        lines[i] = lines[i][:-1]
+
 start = time.time() * 1000000
 
-while True:
-    line = file.readline()
-    if not line:
-        break
-    if "EOF\n" == line:
-        end = time.time() * 1000000
-        time.sleep((1000000 - end + start) / 1000000)
+for line in lines:
+    if "EOF" == line:
+        interval = (1000000 - time.time() * 1000000 + start)
+        if interval > 0 :
+            time.sleep(interval / 1000000)
         start = time.time() * 1000000
         continue
-    # cmd line
-    os.system(line.replace("\n", ""))
+    else:
+        os.system(line)
