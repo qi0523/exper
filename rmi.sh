5c979a912284d30d9ab407bb052a3fa1bddab011
qi0523 <1426042225@qq.comclear>
2022-12-10 16:43:41 +0800

    add rmi.sh

---
+++ rmi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+invokers=$(kubectl get pods -n openwhisk | grep invoker | awk '{print $1}')
+
+array=($invokers)
+
+num=${#array[@]}
+
+for((i=0;i<num;i=$(($i+1))))
+{
+  kubectl exec ${array[i]} -n openwhisk -- /rmi.sh
+}
