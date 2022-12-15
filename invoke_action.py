71e474b2bd6bded28a3708b870e1cf9518f0ab19
qi0523 <1426042225@qq.comclear>
2022-12-16 00:15:13 +0800

    record time

--- invoke_action.py
+++ invoke_action.py
@@ -14,12 +14,13 @@ for i in range(len(lines)):
 
 start = time.time() * 1000000
 
+invoke_start = time.time()
+
 for line in lines:
     if "EOF" == line:
-        interval = (1000000 - time.time() * 1000000 + start)
-        if interval > 0 :
-            time.sleep(interval / 1000000)
-        start = time.time() * 1000000
         continue
     else:
         os.system(line)
+
+invoke_end = time.time()
+print("invoke_start: ", invoke_start, "invoke_end: ", invoke_end)
--- rmi.sh
+++ rmi.sh
@@ -8,5 +8,6 @@ num=${#array[@]}
 
 for((i=0;i<num;i=$(($i+1))))
 {
+  #kubectl exec ${array[i]} -n openwhisk -- crictl rmi b00bf65799d58
   kubectl exec ${array[i]} -n openwhisk -- /rmi.sh
 }
